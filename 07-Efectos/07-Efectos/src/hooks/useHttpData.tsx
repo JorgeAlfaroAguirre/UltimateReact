3af9d7fdfe4a891f36105bd80496ce6c62edb8bc
jorgealfaroaguirre <jorgealfaroaguirre@gmail.com>
2025-02-28 16:44:59 -0300

    Agregando Elementos

---
+++ 07-Efectos/07-Efectos/src/hooks/useHttpData.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+
+export default function useHttpData<T>(url: string) {
+  const [data, setData] = useState<T[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string>();
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const { signal } = controller;
+    async function hook() {
+      try {
+        const response = await fetch(url, { signal });
+        if (!response.ok) throw new Error(`${response.status}`);
+        const data: T[] = await response.json();
+        setData(data);
+        setError(undefined);
+      } catch (error) {
+        setError((error as Error).message);
+      } finally {
+        setLoading(false);
+      }
+    }
+    hook();
+
+    return () => controller.abort();
+  }, []);
+
+  const addData = async (element: T) => {
+    const initialData = [...data];
+    setData([{ id: 0, ...element }, ...data]);
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(element),
+        headers: {
+          "Content-type": "application/json",
+        }, // No es necesario cuando estas utilizando axios, ni con esta API
+      });
+
+      if (!response.ok) {
+        setData(initialData);
+        throw new Error(`${response.status}`);
+      }
+
+      const savedData = await response.json();
+      setData([savedData, ...initialData]);
+    } catch (error) {
+      setError((error as Error).message);
+    }
+  };
+  return { data, loading, error, addData };
+}
